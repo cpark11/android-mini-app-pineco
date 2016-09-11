4ef83049a39e846b5fcde89fc19f7a12411097cd
Eric Xie <xiexieeric@gmail.com>
2016-09-10 22:57:16 -0400

    Milestone 1

---
+++ app/src/main/java/pineco/quizapp/done.java
@@ -0,0 +1,81 @@
+package pineco.quizapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
+
+public class done extends Fragment{
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
+    }
+    private static final int DATASET_COUNT = 2;
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+    protected ArrayList<String> mDataset;
+    private RecyclerView quizList;
+    protected CustomAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    protected LayoutManagerType mCurrentLayoutManagerType;
+    public done() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initDataset();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_todo, container,
+                false); //may need to be done
+        quizList = (RecyclerView) rootView.findViewById(R.id.my_recycler_view);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
+        mAdapter = new CustomAdapter(mDataset);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        quizList.setAdapter(mAdapter);
+        return rootView;
+    }
+    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
+        int scrollPosition = 0;
+
+        // If a layout manager has already been set, get current scroll position.
+        if (quizList.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) quizList.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        quizList.setLayoutManager(mLayoutManager);
+        quizList.scrollToPosition(scrollPosition);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save currently selected layout manager.
+        savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, mCurrentLayoutManagerType);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    /**
+     * Generates Strings for RecyclerView's adapter. This data would usually come
+     * from a local content provider or remote server.
+     */
+    private void initDataset() {
+        mDataset = new ArrayList<String>();
+    }
+}
