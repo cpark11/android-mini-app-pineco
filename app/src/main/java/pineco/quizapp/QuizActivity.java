0274d2120768b7d3f72da00991dd0584d98919d1
Eric Xie <xiexieeric@gmail.com>
2016-09-14 23:04:33 -0400

    temp

--- app/src/main/java/pineco/quizapp/QuizActivity.java
+++ app/src/main/java/pineco/quizapp/QuizActivity.java
@@ -2,6 +2,8 @@ package pineco.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        MyersBriggsQuiz mb = new MyersBriggsQuiz();
+        QuizQuestion q = mb.getQuestions().get(0);
+        TextView question = (TextView) findViewById(R.id.quiz_question);
+        question.setText(q.getText());
+        Button b1 = (Button) findViewById(R.id.b1);
+        b1.setText(q.getAnswers());
     }
 }
